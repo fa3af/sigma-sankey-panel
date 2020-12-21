c4d839c0cf757e95a49f3335892f4c56bb967f20
M. Faishol T. Afandi <faisholtriafandi@gmail.com>
2020-12-21 16:01:11 +0700

    fixing quote

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -1,29 +1,24 @@
-import React from "react";
-import { PanelProps } from "@grafana/data";
-import { SimpleOptions } from "types";
-import { css, cx } from "emotion";
-import { stylesFactory, useTheme } from "@grafana/ui";
-import Chart from "react-google-charts";
+import React from 'react';
+import { PanelProps } from '@grafana/data';
+import { SimpleOptions } from 'types';
+import { css, cx } from 'emotion';
+import { stylesFactory, useTheme } from '@grafana/ui';
+import Chart from 'react-google-charts';
 
 interface Props extends PanelProps<SimpleOptions> {}
 
-export const SimplePanel: React.FC<Props> = ({
-  options,
-  data,
-  width,
-  height
-}) => {
+export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
   const theme = useTheme();
   const styles = getStyles();
   let color: string;
   switch (options.color) {
-    case "red":
+    case 'red':
       color = theme.palette.redBase;
       break;
-    case "green":
+    case 'green':
       color = theme.palette.greenBase;
       break;
-    case "blue":
+    case 'blue':
       color = theme.palette.blue95;
       break;
   }
@@ -41,15 +36,15 @@ export const SimplePanel: React.FC<Props> = ({
   let extraSeries: any[] = [];
 
   data.series[0].fields.forEach((element, index) => {
-    if (element.name === "source.ip") {
+    if (element.name === 'source.ip') {
       primariHeader[0] = element.name;
       sourceIp = element.values;
       sourceIpFlag = true;
-    } else if (element.name === "destination.ip") {
+    } else if (element.name === 'destination.ip') {
       primariHeader[1] = element.name;
       destinationIp = element.values;
       destinationIpFlag = true;
-    } else if (element.name === "Count") {
+    } else if (element.name === 'Count') {
       primariHeader[2] = element.name;
       count = element.values;
       countFlag = true;
@@ -58,31 +53,22 @@ export const SimplePanel: React.FC<Props> = ({
     }
   });
   if (sourceIpFlag && destinationIpFlag && countFlag) {
-    primariHeader[3] = { type: "string", role: "tooltip" };
+    primariHeader[3] = { type: 'string', role: 'tooltip' };
     graphData.push(primariHeader);
-    for (
-      let index = 0;
-      index < data.series[0].fields[0].values.length;
-      index++
-    ) {
+    for (let index = 0; index < data.series[0].fields[0].values.length; index++) {
       const si = sourceIp.get(index);
       const di = destinationIp.get(index);
       const cn = count.get(index);
       const defdata = [si, di, cn];
-      let tooltipText =
-        '<div style="color:#000000; padding: 20px 15px; width:100%"><table style="border:none">';
+      let tooltipText = '<div style="color:#000000; padding: 20px 15px; width:100%"><table style="border:none">';
       tooltipText +=
-        '<tr><td style="white-space: nowrap;">From</td><td style="white-space: nowrap;"> : <b>' +
-        si +
-        "</b></td></tr>";
+        '<tr><td style="white-space: nowrap;">From</td><td style="white-space: nowrap;"> : <b>' + si + '</b></td></tr>';
       tooltipText +=
-        '<tr><td style="white-space: nowrap;">To</td><td  style="white-space: nowrap;"> : <b>' +
-        di +
-        "</b></td></tr>";
+        '<tr><td style="white-space: nowrap;">To</td><td  style="white-space: nowrap;"> : <b>' + di + '</b></td></tr>';
       tooltipText +=
         '<tr><td style="white-space: nowrap;">Count</td><td  style="white-space: nowrap;"> : <b>' +
         cn +
-        "</b></td></tr>";
+        '</b></td></tr>';
       extraSeries.forEach(element => {
         //console.log(element);
         tooltipText +=
@@ -90,14 +76,14 @@ export const SimplePanel: React.FC<Props> = ({
           element.name +
           '</td><td style="white-space: nowrap;"> : <b>' +
           element.values.get(index) +
-          "</b></td></tr>";
+          '</b></td></tr>';
       });
-      tooltipText += "</table></div>";
+      tooltipText += '</table></div>';
 
       graphData.push([...defdata, tooltipText]);
     }
   }
-  console.log("data dari mana tak tahu", graphData);
+  console.log('data dari mana tak tahu', graphData);
 
   // const radii = data.series
   //   .map(series => series.fields.find(field => field.type === 'number'))
@@ -118,25 +104,22 @@ export const SimplePanel: React.FC<Props> = ({
           loader={<div>Loading Chart</div>}
           data={graphData}
           options={{ tooltip: { isHtml: true } }}
-          rootProps={{ "data-testid": "2" }}
+          rootProps={{ 'data-testid': '2' }}
         />
       )}
       {!sourceIpFlag && (
         <div className={styles.textBox}>
-          Source IP tidak ditemukan, mohon menambahkan source.ip pada group by
-          pencarian
+          Source IP tidak ditemukan, mohon menambahkan source.ip pada group by pencarian
         </div>
       )}
       {!destinationIpFlag && (
         <div className={styles.textBox}>
-          Destination IP tidak ditemukan, mohon menambahkan destination.ip pada
-          group by pencarian
+          Destination IP tidak ditemukan, mohon menambahkan destination.ip pada group by pencarian
         </div>
       )}
       {!countFlag && (
         <div className={styles.textBox}>
-          Metric Count tidak ditemukan, mohon menambahkan Count pada metric
-          pencarian
+          Metric Count tidak ditemukan, mohon menambahkan Count pada metric pencarian
         </div>
       )}
       {/*
@@ -173,6 +156,6 @@ const getStyles = stylesFactory(() => {
       bottom: 0;
       left: 0;
       padding: 10px;
-    `
+    `,
   };
 });
--- src/module.ts
+++ src/module.ts
@@ -1,42 +1,40 @@
-import { PanelPlugin } from "@grafana/data";
-import { SimpleOptions } from "./types";
-import { SimplePanel } from "./SimplePanel";
+import { PanelPlugin } from '@grafana/data';
+import { SimpleOptions } from './types';
+import { SimplePanel } from './SimplePanel';
 
-export const plugin = new PanelPlugin<SimpleOptions>(
-  SimplePanel
-).setPanelOptions(builder => {
+export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions(builder => {
   return builder
     .addTextInput({
-      path: "text",
-      name: "Simple text option",
-      description: "Description of panel option",
-      defaultValue: "Default value of text input option"
+      path: 'text',
+      name: 'Simple text option',
+      description: 'Description of panel option',
+      defaultValue: 'Default value of text input option',
     })
     .addBooleanSwitch({
-      path: "showSeriesCount",
-      name: "Show series counter",
-      defaultValue: false
+      path: 'showSeriesCount',
+      name: 'Show series counter',
+      defaultValue: false,
     })
     .addRadio({
-      path: "seriesCountSize",
-      defaultValue: "sm",
-      name: "Series counter size",
+      path: 'seriesCountSize',
+      defaultValue: 'sm',
+      name: 'Series counter size',
       settings: {
         options: [
           {
-            value: "sm",
-            label: "Small"
+            value: 'sm',
+            label: 'Small',
           },
           {
-            value: "md",
-            label: "Medium"
+            value: 'md',
+            label: 'Medium',
           },
           {
-            value: "lg",
-            label: "Large"
-          }
-        ]
+            value: 'lg',
+            label: 'Large',
+          },
+        ],
       },
-      showIf: config => config.showSeriesCount
+      showIf: config => config.showSeriesCount,
     });
 });
